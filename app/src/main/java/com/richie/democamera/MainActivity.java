a5027f82cd9e56badae072bb869f86eff612e3fc
liquan.zhou <liquan.zhou@t2mobile.com>
2017-09-26 17:27:58 +0800

    add permissions

--- app/src/main/java/com/richie/democamera/MainActivity.java
+++ app/src/main/java/com/richie/democamera/MainActivity.java
@@ -12,19 +12,25 @@ import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
 import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.LoginFilter;
 import android.util.Log;
 import android.util.Size;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -33,7 +39,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener, View.OnClickListener {
 
-    public static final String TAG = "DemoCamera";
+    public static final String TAG = "zlq";
 
     private SurfaceTexture mPreview;
 
@@ -53,6 +59,14 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
     private CaptureRequest mCaptureRequest;
     private CameraCaptureSession mCameraCaptureSession;
     private CameraCaptureSession.CaptureCallback mSessionCaptureback;
+    private ImageReader mImageReader;
+
+    private boolean permissionToRecorderAccepted = false;
+    private String[] permissions = {
+//            Manifest.permission.RECORD_AUDIO,
+//            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA};
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
 
     private final CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
         @Override
@@ -78,11 +92,29 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
     };
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecorderAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if(!permissionToRecorderAccepted) finish();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        //获取权限
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
+        Window w = this.getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED, WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+
+        setContentView(R.layout.activity_main);
         mTextureView = (TextureView) findViewById(R.id.ttv_preview);
+        mTextureView.setSurfaceTextureListener(this);
         mButtonStart = (Button) findViewById(R.id.btn_start);
         mButtonStart.setOnClickListener(this);
         mButtonPause = (Button) findViewById(R.id.btn_pause);
@@ -94,13 +126,37 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
 
     }
 
+    private void setupImageReader() {
+        //前三个参数分别是需要的尺寸和格式，最后一个参数代表每次最多获取几帧数据。
+        mImageReader = ImageReader.newInstance(mPreviewSize.getWidth(), mPreviewSize.getHeight(), ImageFormat.JPEG, 2);
+        //监听ImageReader事件，当有图像流数据可用时会回调onImageAvailable方法，它的参数就是预览帧数据，可以对这帧数据进行处理；
+        mImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader imageReader) {
+                Image image = imageReader.acquireLatestImage();
+                ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                byte[] data = new byte[buffer.remaining()];
+                buffer.get(data);
+                image.close();
+            }
+        }, null);
+    }
+
     private void startPreview() throws CameraAccessException {
         mPreview = mTextureView.getSurfaceTexture();
         mPreview.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
         Surface mSurface = new Surface(mPreview);
+
+        setupImageReader();
+        Surface imageReaderSurface = mImageReader.getSurface();
+
+
         mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
         mCaptureRequestBuilder.addTarget(mSurface);
-        mCameraDevice.createCaptureSession(Arrays.asList(mSurface), new CameraCaptureSession.StateCallback() {
+
+        //mCaptureRequestBuilder.addTarget(imageReaderSurface);
+
+        mCameraDevice.createCaptureSession(Arrays.asList(mSurface, imageReaderSurface), new CameraCaptureSession.StateCallback() {
             @Override
             public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
                 mCaptureRequest = mCaptureRequestBuilder.build();
@@ -154,6 +210,7 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
      * @param height
      */
     private void setupCamera(int width, int height) {
+        Log.i(TAG, "setupCamera: ");
         try {
             // 遍历所有摄像头
             for (String cameraId : mCameraManager.getCameraIdList()) {
@@ -194,6 +251,7 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
     @Override
     public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
         // Do somethings when the TextureView is available.
+        Log.i(TAG, "onSurfaceTextureAvailable: ");
         setupCamera(i, i1);
         openCamera();
     }
